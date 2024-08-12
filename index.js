d39c8f457a606b19a90188ab8ee5d46dc9586de8
AshokAshok24 <ashok246445@gmail.com>
2024-08-12 12:10:39 +0530

    index.js

--- index.js
+++ index.js
@@ -100,7 +100,7 @@ app.post('/multipleuploadbysepeate', uploadBySeperate, (req, res) => {
 })
 
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log(`Server running on port http://localhost:${port}`);
